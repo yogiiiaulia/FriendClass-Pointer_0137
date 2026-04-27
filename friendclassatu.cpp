5c2a5ffe2327dbcb49cf8666a8ed8957224bd7eb
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-04-27 18:12:24 +0700

    selesai pembuatan class siswa

--- friendclassatu.cpp
+++ friendclassatu.cpp
@@ -4,5 +4,22 @@ using namespace std;
 
 class siswa;
 class orang{
-    
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
+
 }
+
+class siswa
+{
+    private:
+    int id;
+
+    public:
+    void setId(int pId);
+    void displayAll(orang &a);
+
+};
