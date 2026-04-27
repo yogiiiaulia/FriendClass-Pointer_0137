5159df58678d1aa67501655171d2c5576041457c
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-04-27 18:01:44 +0700

    class pelajar

--- friendfunctiondua.cpp
+++ friendfunctiondua.cpp
@@ -8,4 +8,14 @@ public:
     void showNilaiPelajar(pelajar &x);
 };
 
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar &x);
+};
+
 
