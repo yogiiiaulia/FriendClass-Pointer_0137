1ebc70004c1893323cc8bf0906951336d53b3bec
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-04-27 18:02:06 +0700

    selesai

--- friendfunctiondua.cpp
+++ friendfunctiondua.cpp
@@ -18,4 +18,15 @@ public:
     friend void manusia::showNilaiPelajar(pelajar &x);
 };
 
+void manusia::showNilaiPelajar(pelajar &x)
+{
+    cout << x.nilai;
+}
 
+int main()
+{
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaiPelajar(pbudi);
+    return 0;
+}
